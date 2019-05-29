5b87bc835ad35beefb88adc1d820c169382e320f
Peter Pae <paendabear@gmail.com>
2019-05-29 15:09:53 -0400

    add engine to packson.json

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cors(options));
 
 app.use(helmet());
-app.use(validateBearerToken);
+// app.use(validateBearerToken);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
